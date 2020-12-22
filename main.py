2ec14184bcf7a5e4c7c4f068c64835e29cd21ddc
AlexMartin23 <832007@mystma.org>
2020-12-22 16:11:55 +0000

    unit 1 test try 1

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-#WRITE YOUR CODE IN THIS FILE
+def name(n):
+    return n
+
+def favorites(favCol, favNum):
+    return "Your favorite color is " + favCol + " and your favorite number is " + favNum
+
+def bigAdd(x, y, z, a, b):
+    return x + y + z + a + b
+
+def fourOperations(x, y, z, a, b):
+    return (x+y)-(z*a)/b
+
+
+print (name("Alex"))
+
+print (favorites("green", "7"))
+
+print (bigAdd(2, 5, 1, 9, 5))
+
+print (fourOperations(2, 5, 1, 9, 5))
